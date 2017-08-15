6610a8804010cb12965ef7a1ddfcba043700e8ae
ahaufox <ahaufox@vip.qq.com>
2017-08-15 14:14:34 +0800

    增加后台管理

---
+++ extend/console/console.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * tpAdmin [a web admin based ThinkPHP5]
+ *
+ * @author yuan1994 <tianpian0805@gmail.com>
+ * @link http://tpadmin.yuan1994.com/
+ * @copyright 2016 yuan1994 all rights reserved.
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+namespace console;
+// 加载基础文件
+require __DIR__ . '/../../thinkphp/base.php';
+
+use think\App;
+use think\Console;
+
+// 执行应用
+App::initCommon();
+$cmds = [
+    "console\\command\\Generate",
+    "console\\command\\Db",
+];
+Console::addDefaultCommands($cmds);
+Console::init();
